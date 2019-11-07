a3e0eb1dceee4b529a1e5aa43e088e58df8f0b15
kevincxks <1162367203@qq.com>
2019-11-07 14:22:46 +0800

    lab5

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello")
+	v := []string{"wocao","nima","aiyo"}
+
+	fmt.Println(strings.Join(v, "-"))
 }
